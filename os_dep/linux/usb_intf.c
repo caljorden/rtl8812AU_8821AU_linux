31c82208a6577a10829793f351ab354f168096ac
caljorden <caljorden@hotmail.com>
2015-03-12 22:05:14 -0500

    Added USB id for Trendnet TEW-804UB

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -288,6 +288,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x7392, 0xA812),.driver_info = RTL8821}, /* Edimax - Edimax */
 	{USB_DEVICE(0x2001, 0x3314),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x0846, 0x9052),.driver_info = RTL8821}, /* Netgear - A6100 */
+	{USB_DEVICE(0x20F4, 0x804B),.driver_info = RTL8821}, /* Trendnet - TEW-804UB */
 	{USB_DEVICE(0x0411, 0x0242),.driver_info = RTL8821}, /* BUFFALO - Edimax */
 #endif
 
